3703732efdbf897598d5450a4cf5caf4c6f906af
Together-Games <brianpomeroy@outlook.com>
2025-08-04 12:47:50 -0600

    Delete tapInput.js

--- tapInput.js
+++
@@ -1,8 +0,0 @@
-export function initTapInput(validateProgress) {
-  document.querySelectorAll('.game-button').forEach(button => {
-    const num = button.getAttribute('data-num');
-    button.addEventListener('click', () => {
-      validateProgress(parseInt(num));
-    });
-  });
-}
