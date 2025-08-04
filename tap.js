781acddd6caa248bcefa3c68cf9f9bbf503555ff
Together-Games <brianpomeroy@outlook.com>
2025-08-04 13:27:13 -0600

    Delete tap.js

--- tap.js
+++
@@ -1,8 +0,0 @@
-export function setupTap(buttonIds, checkAction) {
-  buttonIds.forEach(id => {
-    const btn = document.getElementById(id);
-    btn.addEventListener("touchstart", () => {
-      checkAction(`tap-${id.slice(-1)}`);
-    });
-  });
-}
